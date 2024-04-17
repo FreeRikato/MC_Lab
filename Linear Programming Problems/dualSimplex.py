67f3388390d393bfda894b151ddb07b261eab644
FreeRikato <aravinthan@student.tce.edu>
2024-04-17 22:30:37 +0530

    Refactor dualSimplex.py to add a Gradio interface for solving linear programming problems

--- Linear Programming Problems/dualSimplex.py
+++ Linear Programming Problems/dualSimplex.py
@@ -1,13 +1,28 @@
+import gradio as gr
 import lippy as lp
-c_vec = [6, 6, 6]
-a_matrix = [
-    [4, 1, 1],
-    [1, 2, 0],
-    [0, 0.5, 4]
-]
-b_vec = [5, 3, 8]
 
-c_vec, a_matrix, b_vec = lp.primal_to_dual_lp(c_vec, a_matrix, b_vec)
-simplex = lp.SimplexMethod(c_vec, a_matrix, b_vec)
-solution, func_value = simplex.solve()
-print(solution, func_value)
+def solve_lp(c_vec, a_matrix, b_vec):
+    c_vec = [float(i) for i in c_vec.split(',')]
+    a_matrix = [[float(num) for num in row.split(',')] for row in a_matrix.split('\n')]
+    b_vec = [float(i) for i in b_vec.split(',')]
+    
+    c_vec, a_matrix, b_vec = lp.primal_to_dual_lp(c_vec, a_matrix, b_vec)
+    simplex = lp.SimplexMethod(c_vec, a_matrix, b_vec)
+    solution, func_value = simplex.solve()
+    return solution, func_value
+
+iface = gr.Interface(
+    fn=solve_lp,
+    inputs=[
+        gr.Textbox(label="Enter c vector (comma-separated)", value="6, 6, 6"),
+        gr.Textbox(label="Enter A matrix (rows comma-separated, new line for new row)", value="4, 1, 1\n1, 2, 0\n0, 0.5, 4"),
+        gr.Textbox(label="Enter b vector (comma-separated)", value="5, 3, 8")
+    ],
+    outputs=[
+        gr.Textbox(label="Solution"),
+        gr.Textbox(label="Objective Function Value")
+    ],
+    title="Linear Programming Solver"
+)
+
+iface.launch()
