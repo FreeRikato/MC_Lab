2e8e9b97a74b398ed0cc97ba354600d3dfb7030d
FreeRikato <aravinthan@student.tce.edu>
2024-04-17 23:53:26 +0530

    Refactor Vogel's Approximation Method implementation in vogelApproximation.py

--- vogelApproximation.py
+++
@@ -1,7 +0,0 @@
-import transportation_problem as tp
-s = [( 'A1' , 14 ), ( 'A2' , 27 ), ( 'A3' , 19 )]
-d = [( 'B1' , 22 ), ( 'B2' , 13 ), ( 'B3' , 12 ), ( 'B4' , 13 )]
-c = [[ 6 , 7 , 5 , 3 ], [ 8 , 4 , 2 , 7 ], [ 5 , 9 , 10 , 6 ]]
-p = tp . TransportationProblem ( s , d , c )
-r = p . solve ( tp.VogelIniter)
-print ( r )
